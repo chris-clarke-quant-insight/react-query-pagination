90afc3016d54106b5a1c3cd27ca01169e7fb69fd
Chris Clarke <cc@quant-insight.com>
2023-07-21 14:16:55 +0100

    html for not for

--- components/ModelCard.jsx
+++ components/ModelCard.jsx
@@ -66,17 +66,17 @@ function ModelCard (props) {
             <div className="sc-cnMZnH hSByNr">
                 <section className="sc-cCmLNy lmddrv">
                     <input type="radio" id="qv7l6cqy8bST" data-testid="t_st_btn" className="sc-fIhvWL iTAprC" value="st" />
-                    <label for="qv7l6cqy8bST" data-testid="t_st_lbl" className="sc-gkSfol bLGwhT">ST</label>
+                    <label htmlFor="qv7l6cqy8bST" data-testid="t_st_lbl" className="sc-gkSfol bLGwhT">ST</label>
                     <input type="radio" id="aatxwusy4rLT" data-testid="t_lt_btn" className="sc-fIhvWL iTAprC" value="lt" />
-                    <label for="aatxwusy4rLT" data-testid="t_lt_lbl" className="sc-gkSfol bLGwhT">LT</label>
+                    <label htmlFor="aatxwusy4rLT" data-testid="t_lt_lbl" className="sc-gkSfol bLGwhT">LT</label>
                 </section>
                 <section>
                     <input type="radio" id="t1bv4atspq2W" data-testid="t_twoWeeks_btn" className="sc-gzrROc gOPKWb" value="twoWeeks" />
-                    <label for="t1bv4atspq2W" data-testid="t_twoWeeks_lbl" className="sc-fmPOXC hkggfk">2W</label>
+                    <label htmlFor="t1bv4atspq2W" data-testid="t_twoWeeks_lbl" className="sc-fmPOXC hkggfk">2W</label>
                     <input type="radio" id="72kqlbykh11M" data-testid="t_oneMonth_btn" className="sc-gzrROc gOPKWb" value="oneMonth" />
-                    <label for="72kqlbykh11M" data-testid="t_oneMonth_lbl" className="sc-fmPOXC hkggfk">1M</label>
+                    <label htmlFor="72kqlbykh11M" data-testid="t_oneMonth_lbl" className="sc-fmPOXC hkggfk">1M</label>
                     <input type="radio" id="j6b1pdg50o2M" data-testid="t_twoMonths_btn" className="sc-gzrROc gOPKWb" value="twoMonths" />
-                    <label for="j6b1pdg50o2M" data-testid="t_twoMonths_lbl" className="sc-fmPOXC hkggfk">2M</label>
+                    <label htmlFor="j6b1pdg50o2M" data-testid="t_twoMonths_lbl" className="sc-fmPOXC hkggfk">2M</label>
                 </section>
             </div>
         </div>
