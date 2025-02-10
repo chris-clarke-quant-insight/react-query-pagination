133a3fc9bb169decf0b0374e8fa584d9cacbfd22
Chris Clarke <cc@quant-insight.com>
2025-02-10 17:16:25 +0000

    add pins

--- pages/pins/index.jsx
+++ pages/pins/index.jsx
@@ -5,11 +5,24 @@ import Layout from "../../components/Layout";
 import Sort from "../../components/Sort";
 import Search from "../../components/Search";
 import TopSearch from "../../components/TopSearch";
-import OnlyModelList from "../../components/only/OnlyModelList";
 
-import { ListButton } from "../../components/ListButton";
+import * as download from "../../data/pins";
 
-function OnlyPage() {
+function Pin(props){
+    const url = (props.pin.images && props.pin.images['orig'])? props.pin.images['orig'].url : 'no orig';
+
+   return (<div className="grid-container"><a href={url}><img src={url} /></a></div>)
+}
+
+function PinsList(props) {
+    if (!props.pins['default']) {
+        return <p>Loading...</p>;
+    }
+
+    return Object.keys(props.pins['default']).map(pin => <Pin key={pin} pin={props.pins['default'][pin]} />)
+}
+
+function PinsPage() {
   const [searchTerm, setSearchTerm] = useState('');
   const [sort, setSort] = useState('name');
   const [data, setData] = useState({});
@@ -17,12 +30,13 @@ function OnlyPage() {
   useEffect(() => {
     function fetchData() {
       try {
-        const res = fetch(
-            `/api/users`
-        ).then((response) =>{
-          const newData = response.json();
-          newData.then((data) => setData(data));
-        });
+        // const res = fetch(
+        //     `/api/users`
+        // ).then((response) =>{
+        //   const newData = response.json();
+        //   newData.then((data) => setData(data));
+        // });
+        setData(download);
       } catch (err) {
         console.log(err);
       }
@@ -47,17 +61,17 @@ function OnlyPage() {
       <Sort handleChange={handleSort} value={sort} />
     </div></>}>
       <div>
-        <h1>Casual Card list of models{searchTerm ? ` for ${searchTerm}` : ''}
+        <h1>Casual Card list of pins{searchTerm ? ` for ${searchTerm}` : ''}
         </h1>
         <div>
             This is the Only section which is used for experiments
         </div>
         <div className="result-box">
-            {data? <OnlyModelList models={data} />: "Loading..."}
+            {data? <PinsList pins={data} />: "Loading..."}
           </div>
         </div>
     </Layout>
   );
 }
 // Fix Sort 
-export default OnlyPage;
+export default PinsPage;
